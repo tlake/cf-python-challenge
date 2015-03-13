a34a5b2ea67cd86cb59442f7b684f0b5efa13175
tlake <tanner.lake@gmail.com>
2015-03-13 11:10:35 -0700

    initial library; problem statement

---
+++ part1-library/cf-library.py
@@ -0,0 +1,13 @@
+'''
+Use object-oriented Python to model a public library (w/ three classes: Library,
+Shelf, & Book). *  
+
+The library should be aware of a number of shelves. Each shelf should know what
+books it contains. Make the book object have "enshelf" and "unshelf" methods that
+control what shelf the book is sitting on. The library should have a method to
+report all books it contains. Note: this should *not* be a Django (or any other)
+app - just a single file with three classes (plus commands at the bottom showing
+it works) is all that is needed.
+'''
+
+
